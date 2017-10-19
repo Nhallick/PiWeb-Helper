44367203b6f8de1f342b94dd9222bff0bdd76834
Nick Hallick <NickHallick@outlook.com>
2017-10-19 14:14:02 -0400

    Add project files.

---
+++ PiWeb Helper/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tbCustomer = New System.Windows.Forms.TextBox()
+        Me.tbPartNumber = New System.Windows.Forms.TextBox()
+        Me.tbOPN = New System.Windows.Forms.TextBox()
+        Me.lblCustomer = New System.Windows.Forms.Label()
+        Me.lblPartNumber = New System.Windows.Forms.Label()
+        Me.lblOPN = New System.Windows.Forms.Label()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.lbFiles = New System.Windows.Forms.ListBox()
+        Me.btnConfirm = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'tbCustomer
+        '
+        Me.tbCustomer.Location = New System.Drawing.Point(12, 25)
+        Me.tbCustomer.Name = "tbCustomer"
+        Me.tbCustomer.Size = New System.Drawing.Size(247, 20)
+        Me.tbCustomer.TabIndex = 0
+        '
+        'tbPartNumber
+        '
+        Me.tbPartNumber.Location = New System.Drawing.Point(12, 64)
+        Me.tbPartNumber.Name = "tbPartNumber"
+        Me.tbPartNumber.Size = New System.Drawing.Size(247, 20)
+        Me.tbPartNumber.TabIndex = 1
+        '
+        'tbOPN
+        '
+        Me.tbOPN.Location = New System.Drawing.Point(12, 103)
+        Me.tbOPN.Name = "tbOPN"
+        Me.tbOPN.Size = New System.Drawing.Size(247, 20)
+        Me.tbOPN.TabIndex = 2
+        '
+        'lblCustomer
+        '
+        Me.lblCustomer.AutoSize = True
+        Me.lblCustomer.Location = New System.Drawing.Point(12, 9)
+        Me.lblCustomer.Name = "lblCustomer"
+        Me.lblCustomer.Size = New System.Drawing.Size(54, 13)
+        Me.lblCustomer.TabIndex = 3
+        Me.lblCustomer.Text = "Customer:"
+        '
+        'lblPartNumber
+        '
+        Me.lblPartNumber.AutoSize = True
+        Me.lblPartNumber.Location = New System.Drawing.Point(12, 48)
+        Me.lblPartNumber.Name = "lblPartNumber"
+        Me.lblPartNumber.Size = New System.Drawing.Size(69, 13)
+        Me.lblPartNumber.TabIndex = 4
+        Me.lblPartNumber.Text = "Part Number:"
+        '
+        'lblOPN
+        '
+        Me.lblOPN.AutoSize = True
+        Me.lblOPN.Location = New System.Drawing.Point(12, 87)
+        Me.lblOPN.Name = "lblOPN"
+        Me.lblOPN.Size = New System.Drawing.Size(96, 13)
+        Me.lblOPN.TabIndex = 5
+        Me.lblOPN.Text = "Operation Number:"
+        '
+        'btnCheck
+        '
+        Me.btnCheck.Location = New System.Drawing.Point(184, 129)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(75, 23)
+        Me.btnCheck.TabIndex = 6
+        Me.btnCheck.Text = "Check"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'lbFiles
+        '
+        Me.lbFiles.FormattingEnabled = True
+        Me.lbFiles.Location = New System.Drawing.Point(12, 158)
+        Me.lbFiles.Name = "lbFiles"
+        Me.lbFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.lbFiles.Size = New System.Drawing.Size(247, 134)
+        Me.lbFiles.TabIndex = 7
+        '
+        'btnConfirm
+        '
+        Me.btnConfirm.Location = New System.Drawing.Point(184, 298)
+        Me.btnConfirm.Name = "btnConfirm"
+        Me.btnConfirm.Size = New System.Drawing.Size(75, 23)
+        Me.btnConfirm.TabIndex = 8
+        Me.btnConfirm.Text = "Confirm"
+        Me.btnConfirm.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(268, 330)
+        Me.Controls.Add(Me.btnConfirm)
+        Me.Controls.Add(Me.lbFiles)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.lblOPN)
+        Me.Controls.Add(Me.lblPartNumber)
+        Me.Controls.Add(Me.lblCustomer)
+        Me.Controls.Add(Me.tbOPN)
+        Me.Controls.Add(Me.tbPartNumber)
+        Me.Controls.Add(Me.tbCustomer)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "PiWeb Helper"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbCustomer As TextBox
+    Friend WithEvents tbPartNumber As TextBox
+    Friend WithEvents tbOPN As TextBox
+    Friend WithEvents lblCustomer As Label
+    Friend WithEvents lblPartNumber As Label
+    Friend WithEvents lblOPN As Label
+    Friend WithEvents btnCheck As Button
+    Friend WithEvents lbFiles As ListBox
+    Friend WithEvents btnConfirm As Button
+End Class
---
+++ PiWeb Helper/Form1.vb
@@ -0,0 +1,76 @@
+﻿'-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+'Project: PiWeb Helper
+'Description: Software built with the purpose of assisting in the navigation and opening of template files for the PiWeb software in inspection. Built to be used exclusively on the inspection cmm computer
+'Created By: Nick Hallick
+'            Process Engineer
+'            nhallick@strite.com
+'            519-658-9361 ext.344
+'Project Start Date: 12/09/2017
+'Project End Date: 12/09/2017
+'Revision: 1.0.0 (Published)
+
+'***********************************************************************************************************************************************************************
+'The Dreaded TODO Section
+
+
+'***********************************************************************************************************************************************************************
+'-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Imports System.IO
+Public Class Form1
+    Private Sub BtnCheck_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+
+
+        'use tiered if/elseif statement to check if each of the textboxes is empty. If any of them are a messagebox will inform the user and then exit the sub
+        If tbCustomer.Text = "" Then
+            MsgBox("Customer cannot be blank, please enter a customer.", vbCritical, "Error")
+            tbCustomer.Focus()
+            Exit Sub
+        ElseIf tbPartNumber.Text = "" Then
+            MsgBox("Part number cannot be blank, please enter a part number.", vbCritical, "Error")
+            tbPartNumber.Focus()
+            Exit Sub
+        ElseIf tbOPN.Text = "" Then
+            MsgBox("Operation cannot be blank, please enter an operation number.", vbCritical, "Error")
+            tbOPN.Focus()
+            Exit Sub
+        End If
+
+        'set the file location using the info entered by the user in the three textboxes
+        'Dim FileLocation As DirectoryInfo = New DirectoryInfo("C:\Users\nhallick\Desktop\Checksheets\" & tbCustomer.Text & "\" & tbPartNumber.Text & "\" & tbOPN.Text)
+        Dim FileLocation As DirectoryInfo = New DirectoryInfo("C:\Users\Inspection CMM\Desktop\PiWEB Templates\" & tbCustomer.Text & "\" & tbPartNumber.Text & "\" & tbOPN.Text)
+        Dim fi As FileInfo()
+
+        'use try catch method to get all the files from the filelocation directory and store in fi (file info array)
+        Try
+            fi = FileLocation.GetFiles("*.ptx")
+        Catch ex As Exception
+            'if the file directory is typed wrong or not found then throw an error
+            MsgBox("Directory not found, ensure directory exists and all info is typed correctly before trying again.", vbCritical, "Error")
+            Exit Sub
+        End Try
+
+        'clear the listbox of items before adding new ones
+        lbFiles.Items.Clear()
+
+        'loop through each item in the file info array and add each one to the listbox as an item
+        For Each thing In fi
+            lbFiles.Items.Add(thing)
+        Next
+
+    End Sub
+
+    Private Sub BtnConfirm_Click(sender As Object, e As EventArgs) Handles btnConfirm.Click
+        'check to see if the listbox has items selected in it, if it doesnt then tell the user this
+        If lbFiles.SelectedItems.Count = 0 Then
+            MsgBox("Please select at least one file from the list before continuing", vbCritical, "Error")
+        Else
+            'if there are items selected then use the directory string to open the selected files
+            For Each filething In lbFiles.SelectedItems
+                'Process.Start("C:\Users\nhallick\Desktop\Checksheets\" & tbCustomer.Text + "\" & tbPartNumber.Text & "\" & tbOPN.Text & "\" & Convert.ToString(filething))
+                Process.Start("C:\Users\Inspection CMM\Desktop\PiWEB Templates\" & tbCustomer.Text & "\" & tbPartNumber.Text & "\" & tbOPN.Text & "\" & Convert.ToString(filething))
+            Next
+        End If
+
+
+    End Sub
+End Class
